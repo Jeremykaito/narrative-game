94a05c5ece55a379e7af0db93a7adf9bf7c4b615
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-03 02:41:56 +0100

    Pick up object

---
+++ Souvenir_Rose/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    public int id;
+    public bool place; //a place or a object
+
+
+}
---
+++ Souvenir_Rose/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PickUpObject : MonoBehaviour {
+
+    public float range = 15f;
+    public Camera fpsCamera;
+    public Transform releasePos;
+    private bool pickUpObject = false;
+    private RaycastHit target;
+
+    void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(pickUpObject == true)
+            {
+                Release();
+            }
+            PickUp();
+        }
+	}
+
+    private void PickUp()
+    {
+
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out target, range))
+        {
+            InteractiveObject targetObject = target.transform.GetComponent<InteractiveObject>();
+            if (targetObject != null)
+            {
+                if(!targetObject.place)
+                {
+                    target.transform.GetComponent<Rigidbody>().isKinematic = true;
+                    target.transform.position = this.transform.position;
+                    target.transform.parent = fpsCamera.transform;
+                    target.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    pickUpObject = true;
+                }
+            }
+           
+        }
+    }
+
+    private void Release()
+    {
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out target, range))
+        {
+            target.transform.GetComponent<Rigidbody>().isKinematic = false;
+            target.transform.parent = GameObject.Find("Objects").transform;
+            target.transform.position = releasePos.position;
+            pickUpObject = false;
+        }
+    }
+}
