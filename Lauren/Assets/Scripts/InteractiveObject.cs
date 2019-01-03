57cdb3eeae72738001717ec0dc651eae6de3eac5
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-04 00:05:56 +0100

    Début système de placement d'objets

--- Lauren/Assets/Scripts/InteractiveObject.cs
+++ Lauren/Assets/Scripts/InteractiveObject.cs
@@ -7,5 +7,16 @@ public class InteractiveObject : MonoBehaviour
     public int id;
     public bool place; //a place or a object
 
-
+    private void OnTriggerEnter(Collider other)
+    {
+        InteractiveObject targetObject = other.transform.GetComponent<InteractiveObject>();
+        if (targetObject != null)
+        {
+            if(targetObject.id == this.id)
+            {
+                other.transform.GetComponent<Rigidbody>().isKinematic = true;
+                other.enabled = false;
+            } 
+        }
+    }
 }
