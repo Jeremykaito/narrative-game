b67fc2da28a97d599971080210a67a7758a108aa
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-09 19:09:35 +0100

    Test de musique

---
+++ Lauren/Assets/Scripts/Interactions/ChapterZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterZone : MonoBehaviour {
+    private bool activated = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player" && !activated)
+        {
+            AudioManager.instance.Play("CoupDeFil");
+            AudioManager.instance.SetVolume("Theme", 0);
+            activated = true;
+        }
+    }
+}
--- Lauren/Assets/Scripts/Sound/AudioManager.cs
+++ Lauren/Assets/Scripts/Sound/AudioManager.cs
@@ -57,4 +57,10 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Stop();
     }
+
+    public void SetVolume(string name, int vol)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.volume = vol ;
+    }
 }
