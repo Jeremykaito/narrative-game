f4fa66202e8caf24045a993aeec74ffd28944045
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-09 23:40:56 +0100

    Début level manager

---
+++ Lauren/Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    [SerializeField]
+    public Step[] steps;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Lauren/Assets/Scripts/Levels/Step.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Step
+{
+
+    public string name;
+    public string soundName;
+    public GameObject StepGameObject;
+
+}
