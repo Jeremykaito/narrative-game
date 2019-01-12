a1f49a7366cb524320d420109a8dbf78abc50215
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-12 16:52:55 +0100

    test

--- Lauren/Assets/Scripts/Interactions/InteractiveObject.cs
+++ Lauren/Assets/Scripts/Interactions/InteractiveObject.cs
@@ -36,28 +36,4 @@ public class InteractiveObject : MonoBehaviour
     {
         this.gameObject.layer = 10; // Layer InteractiveObject
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Raycast detect an interactive zone
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out target, range, 1 << 11))
-        {
-            InteractiveZone targetZone = other.transform.GetComponent<InteractiveZone>();
-            if (targetZone != null)
-            {
-                // Check if the object is in the right zone
-                if (targetZone.gameObject == CorrectZone)
-                {
-                    other.transform.GetComponent<Rigidbody>().isKinematic = true;
-                    other.transform.position = this.transform.position;
-                    other.transform.rotation = new Quaternion(0, 0, 0, 0);
-                    other.enabled = false;
-
-                    WinParticles.Play();
-                    AudioManager.instance.Play(sound);
-                    LevelManager.instance.StartStep(this.Step);
-                }
-            }
-        }
-    }
 }
