79bf24009985d97e774fda7472ffa7096ad41607
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-13 18:12:43 +0100

    full material test

---
+++ Lauren/Assets/Scripts/FullMaterials.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullMaterials : MonoBehaviour {
+    [SerializeField]
+    private Material fullSceneMaterial;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in this.GetComponentsInChildren<Transform>())
+        {
+
+            if(child.GetComponent<Renderer>() != null)
+            {
+
+                child.GetComponent<Renderer>().material = fullSceneMaterial;
+            }
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
