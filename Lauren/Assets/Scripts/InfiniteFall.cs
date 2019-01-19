46f4d1ca29912a1c4065bc12517149e03c9179dd
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-19 02:09:05 +0100

    leveldesign v1

--- Lauren/Assets/Scripts/InfiniteFall.cs
+++ Lauren/Assets/Scripts/InfiniteFall.cs
@@ -11,11 +11,19 @@ public class InfiniteFall : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (this.transform.position.x < -10)
+        if (this.transform.position.y < -5)
         {
-            this.transform.position += new Vector3(30, 0, 0);
+            this.transform.position += new Vector3(0, 20, 0);
+            this.transform.GetComponent<CustomGravity>().gravityScale = 0;
         }
+
+        if (this.transform.position.y > 10)
+        {
+            this.transform.position = new Vector3(0, 20, 0);
+            this.transform.GetComponent<CustomGravity>().gravityScale = 1;
+        }
+
     }
 }
