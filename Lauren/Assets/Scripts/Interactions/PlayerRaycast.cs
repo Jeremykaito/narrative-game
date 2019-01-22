b080b77191d1048a3ff44f02b719d1bee2880824
Jeremykaito <jeremykaito@hotmail.fr>
2019-01-22 23:56:39 +0100

    fin fin

--- Lauren/Assets/Scripts/Interactions/PlayerRaycast.cs
+++ Lauren/Assets/Scripts/Interactions/PlayerRaycast.cs
@@ -53,7 +53,6 @@ public class PlayerRaycast : MonoBehaviour
         else
         {
             objectParticles.Stop();
-            UIManager.instance.SetReticule(false);
             objectParticles.gameObject.SetActive(false);
         }
     }
---
+++ Lauren/Assets/Scripts/Sound/StartSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSound : MonoBehaviour
+{
+    [SerializeField] private string soundName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.PlaySoundEffect(soundName, this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
